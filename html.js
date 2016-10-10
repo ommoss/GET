c7d9f2ec35f4d338b3755e1d3611cdaaaf69bbc2
Owen Moss <ommoss63@gmail.com>
2016-10-10 20:34:50 +0000

    Cleaned-up

--- html.js
+++ html.js
@@ -2,22 +2,6 @@
 
 var request = require("request");
 var fs = require("fs");
-// requestOptions = {
-//   url: 'http://www.example.com',
-//   method: 'GET'
-
-// };
-
-// request(requestOptions, function(err, res, data){
-//   if(err){
-//     console.log(err);
-//     return false;
-//   }
-//  console.log(data);
-//   // var dprime = HTML.parse(data);
-//   // console.log()
-// });
-
 
 
 function readHTML(name, callback){
@@ -30,13 +14,11 @@ function readHTML(name, callback){
       console.log(err);
       return false;
     }
-   // console.log(data);
    printHTML(data);
 
   });
 }
   function printHTML(htmlData){
-    // var page = htmlData.parse();
     console.log(htmlData);
   }
 
